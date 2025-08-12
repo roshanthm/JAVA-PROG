3fb9b4a0ecbaf1ef149fadda8a92daf89069816b
Roshan Thomas <roshanpattathilsep16@gmail.com>
2025-08-12 22:34:50 +0530

    package

---
+++ Demo/demo.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Demo {
+    public void showMessage() {
+        System.out.println("🚀 Java Packages are awesome!");
+        System.out.println("📦 This message came from the 'demo' package.");
+    }
+}
