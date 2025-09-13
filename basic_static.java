31f20bc2eec3796a2e6b14009f1c136926333667
Roshan Thomas <roshanpattathilsep16@gmail.com>
2025-09-13 13:07:21 +0530

    Create basic_static.java

---
+++ basic_static.java
@@ -0,0 +1,10 @@
+public class phello {
+    static int a=10;
+    public static void main(String[] args) {
+        System.out.println(a);
+        hello(10);
+    }
+    static void hello(int b){
+        System.out.println(b);
+    }
+}
